680dc1c638353f552e9ff01afa9e254b30854a98
cquptcsl <1052111657@qq.com>
2017-01-22 13:41:27 +0800

    first1

---
+++ Dome/meituanshouye.js
@@ -0,0 +1,8 @@
+function changeImage() {
+var element = document.getElementById('star1');
+element.src = "photo/star2.png";
+}
+function changeImage1() {
+var element = document.getElementById('star1');
+element.src = "photo/star1.png";
+}
