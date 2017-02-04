516ecba9be94f85744ecced719d165fefac4156b
cquptcsl <1052111657@qq.com>
2017-02-04 19:29:16 +0800

    first commit

--- Dome/meituanshouye.js
+++
@@ -1,8 +0,0 @@
-function changeImage() {
-var element = document.getElementById('star1');
-element.src = "photo/star2.png";
-}
-function changeImage1() {
-var element = document.getElementById('star1');
-element.src = "photo/star1.png";
-}
