5adab59912d7490aa26885fdf54c59c9338bde38
cquptcsl <1052111657@qq.com>
2017-02-05 21:51:49 +0800

    first commit

---
+++ Domenew/meituanshouye.js
@@ -0,0 +1,344 @@
+function changeImage() {
+    var element = document.getElementById('star1');
+    element.src = "photo/star2.png";
+}
+function changeImage1() {
+    var element = document.getElementById('star1');
+    element.src = "photo/star1.png";
+}
+function looklook() {
+	var look1=document.getElementById('mta-111-h');
+	look1.style.visibility ="visible";
+	var look2=document.getElementById('mta-111-h-1');
+	look2.style.visibility ="hidden";
+	var look3=document.getElementById('mta-111-h-2');
+	look3.style.visibility ="hidden";
+	var look4=document.getElementById('mta-111-h-3');
+	look4.style.visibility ="hidden";
+	var look5=document.getElementById('mta-111-h-4');
+	look5.style.visibility ="hidden";
+	var look6=document.getElementById('mta-111-h-5');
+	look6.style.visibility ="hidden";
+	var look7=document.getElementById('mta-111-h-6');
+	look7.style.visibility ="hidden";
+
+}
+function looklook1() {
+	var look1=document.getElementById('mta-111-h');
+	look1.style.visibility ="hidden";
+}
+function looklook2() {
+	var look2=document.getElementById('mta-111-h-1');
+	look2.style.visibility ="visible";
+	var look1=document.getElementById('mta-111-h');
+	look1.style.visibility ="hidden";
+	var look3=document.getElementById('mta-111-h-2');
+	look3.style.visibility ="hidden";
+	var look4=document.getElementById('mta-111-h-3');
+	look4.style.visibility ="hidden";
+	var look5=document.getElementById('mta-111-h-4');
+	look5.style.visibility ="hidden";
+	var look6=document.getElementById('mta-111-h-5');
+	look6.style.visibility ="hidden";
+	var look7=document.getElementById('mta-111-h-6');
+	look7.style.visibility ="hidden";
+}
+function looklook22() {
+	var look2=document.getElementById('mta-111-h-1');
+	look2.style.visibility ="hidden";
+}
+function looklook3() {
+	var look3=document.getElementById('mta-111-h-2');
+	look3.style.visibility ="visible";
+	var look2=document.getElementById('mta-111-h-1');
+	look2.style.visibility ="hidden";
+	var look1=document.getElementById('mta-111-h');
+	look1.style.visibility ="hidden";
+	var look4=document.getElementById('mta-111-h-3');
+	look4.style.visibility ="hidden";
+	var look5=document.getElementById('mta-111-h-4');
+	look5.style.visibility ="hidden";
+	var look6=document.getElementById('mta-111-h-5');
+	look6.style.visibility ="hidden";
+	var look7=document.getElementById('mta-111-h-6');
+	look7.style.visibility ="hidden";
+}
+function looklook33() {
+	var look3=document.getElementById('mta-111-h-2');
+	look3.style.visibility ="hidden";
+}
+function looklook4() {
+	var look4=document.getElementById('mta-111-h-3');
+	look4.style.visibility ="visible";
+	var look2=document.getElementById('mta-111-h-1');
+	look2.style.visibility ="hidden";
+	var look1=document.getElementById('mta-111-h');
+	look1.style.visibility ="hidden";
+	var look3=document.getElementById('mta-111-h-2');
+	look3.style.visibility ="hidden";
+	var look5=document.getElementById('mta-111-h-4');
+	look5.style.visibility ="hidden";
+	var look6=document.getElementById('mta-111-h-5');
+	look6.style.visibility ="hidden";
+	var look7=document.getElementById('mta-111-h-6');
+	look7.style.visibility ="hidden";
+}
+function looklook44() {
+	var look4=document.getElementById('mta-111-h-3');
+	look4.style.visibility ="hidden";
+}
+function looklook5() {
+	var look5=document.getElementById('mta-111-h-4');
+	look5.style.visibility ="visible";
+	var look2=document.getElementById('mta-111-h-1');
+	look2.style.visibility ="hidden";
+	var look1=document.getElementById('mta-111-h');
+	look1.style.visibility ="hidden";
+	var look3=document.getElementById('mta-111-h-2');
+	look3.style.visibility ="hidden";
+	var look4=document.getElementById('mta-111-h-3');
+	look4.style.visibility ="hidden";
+	var look6=document.getElementById('mta-111-h-5');
+	look6.style.visibility ="hidden";
+	var look7=document.getElementById('mta-111-h-6');
+	look7.style.visibility ="hidden";
+}
+function looklook55() {
+	var look5=document.getElementById('mta-111-h-4');
+	look5.style.visibility ="hidden";
+}
+function looklook6() {
+	var look6=document.getElementById('mta-111-h-5');
+	look6.style.visibility ="visible";
+	var look2=document.getElementById('mta-111-h-1');
+	look2.style.visibility ="hidden";
+	var look1=document.getElementById('mta-111-h');
+	look1.style.visibility ="hidden";
+	var look3=document.getElementById('mta-111-h-2');
+	look3.style.visibility ="hidden";
+	var look4=document.getElementById('mta-111-h-3');
+	look4.style.visibility ="hidden";
+	var look5=document.getElementById('mta-111-h-4');
+	look5.style.visibility ="hidden";
+	var look7=document.getElementById('mta-111-h-6');
+	look7.style.visibility ="hidden";
+}
+function looklook66() {
+	var look6=document.getElementById('mta-111-h-5');
+	look6.style.visibility ="hidden";
+}
+function looklook7() {
+	var look7=document.getElementById('mta-111-h-6');
+	look7.style.visibility ="visible";
+	var look2=document.getElementById('mta-111-h-1');
+	look2.style.visibility ="hidden";
+	var look1=document.getElementById('mta-111-h');
+	look1.style.visibility ="hidden";
+	var look3=document.getElementById('mta-111-h-2');
+	look3.style.visibility ="hidden";
+	var look4=document.getElementById('mta-111-h-3');
+	look4.style.visibility ="hidden";
+	var look5=document.getElementById('mta-111-h-4');
+	look5.style.visibility ="hidden";
+	var look6=document.getElementById('mta-111-h-5');
+	look6.style.visibility ="hidden";
+}
+function looklook77(){
+	var look7=document.getElementById('mta-111-h-6');
+	look7.style.visibility ="hidden";
+}
+function move11() {
+	var move1=document.getElementById('a_1');
+	move1.style.visibility ="hidden";
+}
+function move1() {
+	var move1=document.getElementById('a_1');
+	move1.style.visibility ="visible";
+	var move7=document.getElementById('a_7');
+	move7.style.visibility ="hidden";
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="hidden";
+	var move3=document.getElementById('a_3');
+	move3.style.visibility ="hidden";
+	var move4=document.getElementById('a_4');
+	move4.style.visibility ="hidden";
+	var move5=document.getElementById('a_5');
+	move5.style.visibility ="hidden";
+	var move6=document.getElementById('a_6');
+	move6.style.visibility ="hidden";
+}
+function move22() {
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="hidden";
+}
+function move2() {
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="visible";
+	var move7=document.getElementById('a_7');
+	move7.style.visibility ="hidden";
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="hidden";
+	var move3=document.getElementById('a_3');
+	move3.style.visibility ="hidden";
+	var move4=document.getElementById('a_4');
+	move4.style.visibility ="hidden";
+	var move5=document.getElementById('a_5');
+	move5.style.visibility ="hidden";
+	var move6=document.getElementById('a_6');
+	move6.style.visibility ="hidden";
+	var move1=document.getElementById('a_1');
+	move1.style.visibility ="hidden";
+}
+function move33() {
+	var move3=document.getElementById('a_3');
+	move3.style.visibility ="hidden";
+}
+function move3() {
+	var move3=document.getElementById('a_3');
+	move3.style.visibility ="visible";
+	var move7=document.getElementById('a_7');
+	move7.style.visibility ="hidden";
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="hidden";
+	var move4=document.getElementById('a_4');
+	move4.style.visibility ="hidden";
+	var move5=document.getElementById('a_5');
+	move5.style.visibility ="hidden";
+	var move6=document.getElementById('a_6');
+	move6.style.visibility ="hidden";
+	var move1=document.getElementById('a_1');
+	move1.style.visibility ="hidden";
+}
+function move44() {
+	var move4=document.getElementById('a_4');
+	move4.style.visibility ="hidden";
+}
+function move4() {
+	var move4=document.getElementById('a_4');
+	move4.style.visibility ="visible";
+	var move7=document.getElementById('a_7');
+	move7.style.visibility ="hidden";
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="hidden";
+	var move3=document.getElementById('a_3');
+	move3.style.visibility ="hidden";
+	var move5=document.getElementById('a_5');
+	move5.style.visibility ="hidden";
+	var move6=document.getElementById('a_6');
+	move6.style.visibility ="hidden";
+	var move1=document.getElementById('a_1');
+	move1.style.visibility ="hidden";
+}
+function move55() {
+	var move5=document.getElementById('a_5');
+	move5.style.visibility ="hidden";
+}
+function move5() {
+	var move5=document.getElementById('a_5');
+	move5.style.visibility ="visible";
+	var move7=document.getElementById('a_7');
+	move7.style.visibility ="hidden";
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="hidden";
+	var move3=document.getElementById('a_3');
+	move3.style.visibility ="hidden";
+	var move4=document.getElementById('a_4');
+	move4.style.visibility ="hidden";
+	var move6=document.getElementById('a_6');
+	move6.style.visibility ="hidden";
+	var move1=document.getElementById('a_1');
+	move1.style.visibility ="hidden";
+}
+function move66() {
+	var move6=document.getElementById('a_6');
+	move6.style.visibility ="hidden";
+}
+function move6() {
+	var move6=document.getElementById('a_6');
+	move6.style.visibility ="visible";
+	var move7=document.getElementById('a_7');
+	move7.style.visibility ="hidden";
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="hidden";
+	var move3=document.getElementById('a_3');
+	move3.style.visibility ="hidden";
+	var move4=document.getElementById('a_4');
+	move4.style.visibility ="hidden";
+	var move5=document.getElementById('a_5');
+	move5.style.visibility ="hidden";
+	var move1=document.getElementById('a_1');
+	move1.style.visibility ="hidden";
+}
+function move77() {
+	var move7=document.getElementById('a_7');
+	move7.style.visibility ="hidden";
+}
+function move7() {
+	var move7=document.getElementById('a_7');
+	move7.style.visibility ="visible";
+	var move2=document.getElementById('a_2');
+	move2.style.visibility ="hidden";
+	var move3=document.getElementById('a_3');
+	move3.style.visibility ="hidden";
+	var move4=document.getElementById('a_4');
+	move4.style.visibility ="hidden";
+	var move5=document.getElementById('a_5');
+	move5.style.visibility ="hidden";
+	var move6=document.getElementById('a_6');
+	move6.style.visibility ="hidden";
+	var move1=document.getElementById('a_1');
+	move1.style.visibility ="hidden";
+}
+window.onload = function () {
+    var container = document.querySelector("#container");
+    var showimg = document.querySelector("#showing");
+    var circles = 5;
+    var clickFlag=true;
+    var time;
+    var index=0;
+    var Distance=container.offsetWidth;
+
+    function AutoGo(){
+        var start=showimg.offsetLeft;
+        var end=index*Distance*(-1);
+        var change=end-start;
+        var timer;
+        var t=0;
+        var maxT=10;
+        clearInterval(timer);
+        timer=setInterval(function(){
+            t++;
+            if(t>=maxT){
+                clearInterval(timer);
+                clickFlag=true;
+            }
+            showimg.style.left=change/maxT*t+start+"px";
+            if(index==5&&t>=maxT){
+                showimg.style.left=0;
+                index=0;
+            }
+        },100);
+    }
+    function forward(){
+        index++;
+        if(index>5){
+            index=0;
+        }
+        AutoGo();
+    }
+    function backward(){
+        index--;
+        if(index<0){
+            index=4;
+            showimg.style.left=(index+1)*Distance*(-1)+"px";
+        }
+        AutoGo();
+    }
+    time=setInterval(forward,3000);
+    container.onmouseover=function(){
+        clearInterval(time);
+    }
+    container.onmouseout=function(){
+        time=setInterval(forward,3000); 
+    }
+}
